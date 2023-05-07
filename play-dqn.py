36a78026e2d84b066ccc38042a35482f12d0944e
Scott Hoge <scott.hoge@gmail.com>
2023-05-07 13:51:56 -0400

    more cleanup

--- play-dqn.py
+++ play-dqn.py
@@ -4,7 +4,8 @@ from stable_baselines3.common.env_util import make_atari_env
 
 env = make_atari_env('ALE/Frogger-v5', n_envs=4)
 
-model = DQN.load("./Frogger-v5-dqn-10000000.0-zoo-2023-04-21-16:12:33.261800 - best/best_model")
+#model = DQN.load("./Frogger-v5-dqn-10000000.0-zoo-2023-04-21-16:12:33.261800 - best/best_model")
+model = DQN.load("dqn-frogger")
 
 obs = env.reset()
 
